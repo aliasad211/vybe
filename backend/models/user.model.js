bb5e04245c879d7572a08500ccbc52326fdef095
aliasad211 <info.aliasad785@gmail.com>
2026-07-07 02:31:24 +0500

    made server and models

---
+++ backend/models/user.model.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    userName:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    Password:{
+        type:String,
+        required:true
+    },
+    profileImage:{
+        type:String,
+    },
+    followers:[
+        {types:mongoose.Schema.Types.ObjectId,
+         ref:"User"
+        }
+    ],
+    following:[
+        {types:mongoose.Schema.Types.ObjectId,
+         ref:"User"
+        }
+    ],
+    posts:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Post"
+        }
+    ],
+    saved:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Post"
+        }
+    ],
+    loops:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Loop"
+        }
+    ],
+    story:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Story"
+        }
+    ],
+},{timestamps:true})
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
