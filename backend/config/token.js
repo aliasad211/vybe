521f71eb1c52adbe59e931f51529b32daa10b014
aliasad211 <info.aliasad785@gmail.com>
2026-07-08 00:18:13 +0500

    generated jwt token

---
+++ backend/config/token.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+const genToken=async(userId)=>{
+    try{
+    const token = await jwt.sign({userId}, process.env.JWT_SECRET,{expiresIn:"10y"});
+    return token;
+
+    }catch(error){
+      return res.status(500).json(`generate token err ${error}`)
+    }
+}
+
+export default genToken;
