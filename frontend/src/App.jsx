1ada4e69e81733ce6f1b7b91c42723b1fd38537d
aliasad211 <info.aliasad785@gmail.com>
2026-07-14 20:07:16 +0500

    made route protected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,19 @@ import { Route, Routes } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import ForgotPassword from './pages/ForgotPassword'
+import Home from './pages/Home'
+import { useSelector } from 'react-redux'
 
 export const serverUrl = "http://localhost:8000";
 
 function App() {
+  const {userData} = useSelector(state => state.user);
   return (
     <Routes>
-      <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/signin' element={<SignIn/>}/>
-      <Route path='/forgot-password' element={<ForgotPassword/>}/>
+      <Route path='/' element={userData?<Home/>:<SignIn/>}/>
+      <Route path='/signup' element={!userData?<SignUp/>:<Home/>}/>
+      <Route path='/signin' element={!userData?<SignIn/>:<Home/>}/>
+      <Route path='/forgot-password' element={!userData?<ForgotPassword/>:<Home/>}/>
     </Routes>
   )
 }
