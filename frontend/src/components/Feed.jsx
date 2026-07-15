aaf944688b84ec0bb82f14ccf069d84bf2f141d9
aliasad211 <info.aliasad785@gmail.com>
2026-07-16 02:07:06 +0500

    made feed section and nav

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { FaRegHeart } from "react-icons/fa";
 import logo2 from "../assets/logo2.png";
-import StoryDp from './StoryDp';
+import StoryDp from './StoryDp.jsx';
+import Nav from './Nav.jsx';
 
 export default function Feed() {
     return (
-        <div className='lg:w-[50%] w-full bg-black min-h-screen lg:h-screen relative lg:overflow-y-auto'>
+        <div className='lg:w-[50%] w-full bg-black min-h-screen lg:h-screen relative lg:overflow-y-auto hide-scrollbar'>
             <div className='lg:hidden md:block w-full h-25 flex items-center justify-between p-5'>
                 <img src={logo2} alt='' className='w-20' />
                 <div>
@@ -28,6 +29,10 @@ export default function Feed() {
               <StoryDp userName={"asad"}/>
               <StoryDp userName={"asad"}/>
 
+            </div>
+
+            <div className='w-full min-h-screen flex flex-col items-center gap-5 p-2.5 pt-10 bg-white rounded-t-[60px] relative pb-30'>
+               <Nav/>
             </div>
         </div>
     )
