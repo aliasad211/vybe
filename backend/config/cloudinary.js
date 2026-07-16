475eb0fa1a344c526db136e71e960dd8e5f67d3d
aliasad211 <info.aliasad785@gmail.com>
2026-07-17 02:07:41 +0500

    setup middleware

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,12 +1,23 @@
 import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
-const uploadOnCloudinary=async(file)=>{
-    cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
-});
-cloudinary.uploader.upload(file,{
-    resource_type:'auto'
-})
+const uploadOnCloudinary = async (file) => {
+    try {
+        cloudinary.config({
+            cloud_name: process.env.CLOUD_NAME,
+            api_key: process.env.API_KEY,
+            api_secret: process.env.API_SECRET,
+        });
+        const result = await cloudinary.uploader.upload(file, {
+            resource_type: 'auto'
+        })
+        fs.unlinkSync(file);
+        return result.secure_url
+    } catch (error) {
+       console.log(error);
+       fs.unlinkSync(file);
+    }
+
 }
+
+export default uploadOnCloudinary;
