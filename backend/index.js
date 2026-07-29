6b7dbf426c961d6519d4f8f129557a8a7dad795e
aliasad211 <info.aliasad785@gmail.com>
2026-07-29 12:20:02 +0500

    story routes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import postRouter from "./routes/post.routes.js";
 import loopRouter from "./routes/loop.routes.js";
+import storyRouter from "./routes/story.routes.js";
 
 dotenv.config();
 const app = express();
@@ -23,6 +24,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
 app.use("/api/loop", loopRouter);
+app.use("/api/story", storyRouter);
 
 app.listen(port,()=>{
     connectDb();
