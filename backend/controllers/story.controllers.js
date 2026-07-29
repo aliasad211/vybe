5c560231289bcfe3186edc02bfa1d4dd3b6952b8
aliasad211 <info.aliasad785@gmail.com>
2026-07-29 10:56:47 +0500

    viewStory controller

--- backend/controllers/story.controllers.js
+++ backend/controllers/story.controllers.js
@@ -30,3 +30,29 @@ export const uploadStory = async(req,res)=>{
        return res.status(500).json({message:"upload story error"})
     }
 }
+
+export const viewStory = async(req,res)=>{
+    try{
+       const storyId = req.params.storyId;
+       const story = await Story.findById(storyId)
+
+       if(!story){
+         return res.status(400).json({message:"story not found"})
+       }
+
+       const viewersIds = story.viewers.map(id=>id.toString())
+
+       if(!viewersIds.includes(req.userId.toString())){
+        story.viewers.push(req.userId)
+        await story.save()
+       }
+
+       const populatedStory = await Story.findById(story._id)
+       .populate("author","name  userName profileImage")
+       .populate("viewers","name userName profileImage")
+
+       return res.status(200).json(populatedStory)
+    }catch(error){
+        return res.status(500).json({message:"story view error"})
+    }
+}
