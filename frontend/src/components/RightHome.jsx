42ef591a2b4e25c104d5f3d776775fa652abd678
aliasad211 <info.aliasad785@gmail.com>
2026-08-07 21:48:38 +0500

    feat(frontend): render chat panel in RightHome
    RightHome now toggles between the conversation list and the open
    chat thread instead of the placeholder text.

--- frontend/src/components/RightHome.jsx
+++ frontend/src/components/RightHome.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import ConversationList from './ConversationList'
+import ChatWindow from './ChatWindow'
 
 function RightHome() {
+  const { selectedUser } = useSelector(state => state.message)
+
   return (
     <div className='w-[25%] hidden lg:block min-h-screen bg-black border-l-2 border-gray-700'>
-        RightHome
+        {selectedUser ? <ChatWindow/> : <ConversationList/>}
     </div>
   )
 }
